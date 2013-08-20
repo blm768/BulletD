2ee3a1790a40e7549796d9afb4dcaf04f93d307c
Ben <blm768@gmail.com>
2013-08-19 21:24:57 -0700

    Removed some unused code

--- bullet/bindings/types.d
+++ bullet/bindings/types.d
@@ -23,7 +23,7 @@ template dType(T: RefParam!T) {
 	enum dType = "ref " ~ dType!T;
 }
 
-//To do: how to handle shared?
+//TODO: how to handle shared?
 template cppType(T) {
 	static if(!isMutable!T) {
 		enum cppType = "const " ~ cppType!(Unqual!T);
@@ -50,23 +50,6 @@ template cppTypes(Types ...) {
 		alias cppType!(Types[0]) cppTypes;
 	} else {
 		enum cppTypes = cppType!(Types[0]) ~ ", " ~ cppTypes!(Types[1 .. $]);
-	}
-}
-
-template cppCompatibleType(T) {
-	static if(isArray!T) {
-		alias typeof(T[0])* cppCompatibleType;
-	} else {
-		alias T cppCompatibleType;
-	} 
-}
-
-template cppCompatibleTypes(Types ...) {
-	static assert(Types.length > 0);
-	static if(Types.length == 1) {
-		enum cppCompatibleTypes = cppCompatibleType!(Types[0]).stringof;
-	} else {
-		enum cppCompatibleTypes = cppCompatibleType!(Types[0]).stringof ~ ", " ~ cppCompatibleTypes!(Types[1 .. $]);
 	}
 }
 
