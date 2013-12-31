181fd8989558049409134fdb3d07b94d190fe3f4
vuaru <gerbrand@meinmein.com>
2013-12-31 18:07:15 +0100

    Add license.

--- bullet/BulletCollision/CollisionShapes/btConvexInternalShape.d
+++ bullet/BulletCollision/CollisionShapes/btConvexInternalShape.d
@@ -1,3 +1,14 @@
+// BulletD - a D binding for the Bullet Physics engine
+// written in the D programming language
+//
+// Copyright: Ben Merritt 2012 - 2013,
+//            MeinMein 2013 - 2014.
+// License:   Boost License 1.0
+//            (See accompanying file LICENSE_1_0.txt or copy at
+//             http://www.boost.org/LICENSE_1_0.txt)
+// Authors:   Ben Merrit,
+//            Gerbrand Kamphuis (meinmein.com).
+
 module bullet.BulletCollision.CollisionShapes.btConvexInternalShape;
 
 import bullet.bindings.bindings;
